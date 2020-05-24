fc0bb42d6f6d0709d2f8953ecc897c4e44980465
Jeroen de Bruijn <vidavidorra+jdbruijn@gmail.com>
2020-05-24 15:16:37 +0200

    feat: add Coap class showing the interface

--- src/coap.cpp
+++ src/coap.cpp
@@ -1,22 +1,11 @@
-#include "code.hpp"
+#include "coap.hpp"
 
 namespace coap {
 
-/**
- * I want a fluent-type interface so I can do something like the following. The
- * data should be externally defined so its easy to manage externally.
- *
- * coap(data, type, code, message_id)
- *   .token(0x1234)
- *   .option(2109, {0xaa, 0x55})
- *   .option(10, 5)
- *   .option(Option::kUriPath, "/test/file.txt")
- *   .payload({0x00, 0x11, 0x22, 0x33, 0x44, 0x55})
- */
+Coap::Coap() {}
 
-class Coap {
- public:
-  Coap() = default;
-};
+Coap::Coap& Coap::token() { return *this; }
+Coap::Coap& Coap::option() { return *this; }
+Coap::Coap& Coap::payload() { return *this; }
 
 }  // namespace coap
---
+++ src/coap.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+namespace coap {
+
+/**
+ * I want a fluent-type interface so I can do something like the following. The
+ * data should be externally defined so its easy to manage externally.
+ *
+ * Coap coap(type, code, message_id);
+ * coap.token(0x1234)
+ *   .option(2109, {0xaa, 0x55})
+ *   .option(10, 5)
+ *   .option(Option::kUriPath, "/test/file.txt")
+ *   .payload({0x00, 0x11, 0x22, 0x33, 0x44, 0x55})
+ */
+
+class Coap {
+ public:
+  /**
+   * - Constructor from data, i.e. a byte vector.
+   */
+  Coap();
+
+  Coap& token();
+  Coap& option();
+  Coap& payload();
+};
+
+}  // namespace coap
